8bcd7cc0743c591e084011898ede705decb12995
rwema3 <rwemadominik3@gmail.com>
2022-02-22 10:11:40 +0200

    Update math_util.dart

--- lib/src/math_util.dart
+++ lib/src/math_util.dart
@@ -362,7 +362,7 @@ pression = MathUtil.getMultiplySignExp(1, 10);
   }
 
   static List<Expression> te(int level, int count) {
-    var 
+    
     int min = level == 1 ? 1 : (5 * level) - 5; //1 5 10 15 20 25
     int max =? 10 : (10 * level); //10 20 30 40 50 60
     print("$min $max");
