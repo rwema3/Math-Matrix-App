be4dcf9c2665597047a01f3e339ea5f388e88ea2
rwema3 <rwemadominik3@gmail.com>
2022-02-22 09:56:55 +0200

    Update math_util.dart

--- lib/src/math_util.dart
+++ lib/src/math_util.dart
@@ -354,7 +354,7 @@ class MathUtil {
     int min = level == 1 ? 1 : (5 * level) - 5; //1 5 10 15 20 25
     int max = level == 1 ? 10 : (10 * level); //10 20 30 40 50 60
     print("$min $max");
-    while (listength < cnt) {
+    while th < cnt) {
       MathUtil.erateRandomSign1(count - list.length).forEach((String sign) {
         Expressio
         if (level <= 2)
