c697996dcd8b15ff382fd15d8ebf34894b37ccba
rwema3 <rwemadominik3@gmail.com>
2022-02-22 10:05:42 +0200

    Update math_util.dart

--- lib/src/math_util.dart
+++ lib/src/math_util.dart
@@ -353,7 +353,7 @@ class MathUtil {
 pression = MathUtil.getMultiplySignExp(1, 10);
               break;
             
-        exprMathUtil.getDivideSignExp(1, 10);
+        exMathUtil.getDivideSignExp(1, 10);
                  (level <= 3) {
 (si
               thUtilPlusSignExp(min, max);
