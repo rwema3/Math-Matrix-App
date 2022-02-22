ecebb5c78569c9ee00e1576b24260d2928b29c58
rwema3 <rwemadominik3@gmail.com>
2022-02-22 10:25:46 +0200

    Update math_util.dart

--- lib/src/math_util.dart
+++ lib/src/math_util.dart
@@ -363,7 +363,7 @@ pression = MathUtil.getMultiplySignExp(1, 1
     return ieranderand, operator2: $operator2, thirdOperand: $thirdOperand, answer: $
   @override
   bool operator 
-Expression
+
           runtimeTe =thruntimeType &&
           firstOperand == other.firstOperand &&
           operator1  other.opetor1 &&
