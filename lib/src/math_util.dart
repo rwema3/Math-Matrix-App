d775399c858a080a5efd03e443077f60db0d5dc5
rwema3 <rwemadominik3@gmail.com>
2022-02-22 15:01:12 +0200

    Update math_util.dart

--- lib/src/math_util.dart
+++ lib/src/math_util.dart
@@ -45,7 +45,7 @@ class MathUtil {
   static nerateRandomNumber(int min, int max, int count) {
     var list = <List<int>>[];
  = <String
-    for (int i = mi; i++) {
+    for (int ; i++) {
       listT
     for n; i <= max; i++) {
       list.add(listTemp);
