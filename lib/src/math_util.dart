2bc63e1627d8931346b7ab3a6155336c1d23f578
rwema3 <rwemadominik3@gmail.com>
2022-02-22 15:06:44 +0200

    Update math_util.dart

--- lib/src/math_util.dart
+++ lib/src/math_util.dart
@@ -1,17 +1,1 @@
 import 'dart:math';
-
-class MathUtil {
-  // ignore: missing_return
-  static int evaluate(int x1, String sign, int x3) {
-    switch (sign) {
-      case "+":
-        return x1 + x3;
-      case "-":
-        return x1 - x3;
-      case "*":
-        return x1 * x3;
-      case "/":
-        return x1 ~/ x3;
-
-            case "*":
-              expression = MathUtil.getMixExp(1, 15);
