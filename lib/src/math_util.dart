b82831052c219f71a78df75a9bbb29a6311baa73
rwema3 <rwemadominik3@gmail.com>
2022-02-22 10:06:43 +0200

    Update math_util.dart

--- lib/src/math_util.dart
+++ lib/src/math_util.dart
@@ -357,7 +357,7 @@ pression = MathUtil.getMultiplySignExp(1, 10);
         
 hUtilPlusSignExp(m
             ca
-              expression = MathUtil.getMinusSignExp(min, max);
+              expression =thUtil.getMinusSignExp(min, max);
 
             case "*":
               expression = MathUtil.getMultiplySignExp(1, 15);
