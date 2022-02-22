f697144bf5a463e82a0fdffe8b83eab3b598a845
rwema3 <rwemadominik3@gmail.com>
2022-02-22 10:14:55 +0200

    Update math_util.dart

--- lib/src/math_util.dart
+++ lib/src/math_util.dart
@@ -361,7 +361,7 @@ pression = MathUtil.getMultiplySignExp(1, 10);
     return lis
     int  = 1 ? 1 : vel) - 5; //1 5 10 15 20 25
     
-lig
+
       generateRandomSi(count - list.length).forEach((String sign) {
          expression;
         if (level <= 2) {
