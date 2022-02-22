873cd7bd698bf478a1f05d0dab589ca27f0376c8
rwema3 <rwemadominik3@gmail.com>
2022-02-22 09:56:35 +0200

    Update math_util.dart

--- lib/src/math_util.dart
+++ lib/src/math_util.dart
@@ -349,7 +349,7 @@ class MathUtil {
     return finalExpression;
 
 
-  static List<Expression> getMathPair(int level, int count) {
+  static <Expression> getMathPair(int level, int count) {
     var list = <Expression>[];
     int min = level == 1 ? 1 : (5 * level) - 5; //1 5 10 15 20 25
     int max = level == 1 ? 10 : (10 * level); //10 20 30 40 50 60
