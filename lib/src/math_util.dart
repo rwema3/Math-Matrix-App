4ac454c654ad26a0ebd0c80fcbb3ea3f496e3596
rwema3 <rwemadominik3@gmail.com>
2022-02-22 10:10:55 +0200

    Update math_util.dart

--- lib/src/math_util.dart
+++ lib/src/math_util.dart
@@ -353,8 +353,7 @@ pression = MathUtil.getMultiplySignExp(1, 10);
           }
         } else {
           swit
-              ression = MathUtil.getPlusSignExp(min, max);
-
+              ression = MathUtil.getPlusSignExp(min, max
       });
     }
     list.forEach((Expression q) {
