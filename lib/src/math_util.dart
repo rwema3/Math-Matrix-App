e943fb59d0305db7a11b08cde09ca5dd23de6af7
rwema3 <rwemadominik3@gmail.com>
2022-02-22 10:03:37 +0200

    Update math_util.dart

--- lib/src/math_util.dart
+++ lib/src/math_util.dart
@@ -349,7 +349,7 @@ class MathUtil {
     return finalExpressio
 
   static <r(int level, int count) {
-    var list ession>[];
+    var list ];
 pression = MathUtil.getMultiplySignExp(1, 10);
               break;
             case "/":
