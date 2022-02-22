46c1beb11cd804e817cf51b8d523f735ed1aebe8
rwema3 <rwemadominik3@gmail.com>
2022-02-22 10:15:28 +0200

    Update math_util.dart

--- lib/src/math_util.dart
+++ lib/src/math_util.dart
@@ -358,7 +358,7 @@ pression = MathUtil.getMultiplySignExp(1, 10);
       print("${q.toString()}");
     });
 
-    int   1 : vel) - 5; //1 5 10 15 20 
+    int   1 : /1 5 10 15 20 
       list.length).forEach((String sign) {
          
         if (level <= 2) {
