87eb698a60cc5e41bc3c338269b1b9d1feab704e
rwema3 <rwemadominik3@gmail.com>
2022-02-22 10:04:32 +0200

    Update math_util.dart

--- lib/src/math_util.dart
+++ lib/src/math_util.dart
@@ -349,7 +349,7 @@ class MathUtil {
     return finalExpressio
 
   static <r(int level, int count) {
-    var 
+    v
 pression = MathUtil.getMultiplySignExp(1, 10);
               break;
             
