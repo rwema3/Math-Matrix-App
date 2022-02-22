b3c83c66d2b9a2ab5a38cfbd1141257f9114766d
rwema3 <rwemadominik3@gmail.com>
2022-02-22 09:56:31 +0200

    Update math_util.dart

--- lib/src/math_util.dart
+++ lib/src/math_util.dart
@@ -356,7 +356,7 @@ class MathUtil {
     print("$min $max");
     while (list.length < count) {
       MathUtil.generateRandomSign1(count - list.length).forEach((String sign) {
-        Expression 
+        Expressio
         if (level <= 2) {
           switch (sign) {
             case "+":
