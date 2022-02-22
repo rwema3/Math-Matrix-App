bb45e6526d078ee938992d1ea229ba941c4576de
rwema3 <rwemadominik3@gmail.com>
2022-02-22 14:59:46 +0200

    Update math_util.dart

--- lib/src/math_util.dart
+++ lib/src/math_util.dart
@@ -45,7 +45,7 @@ class MathUtil {
       ',
     while (listOfSign.length < count) {
       list[row][colOfSign.last)
-        listOfSigncol
+
   }
 
   static List<String> generateRandomNumber(int min, int max, int count) {
