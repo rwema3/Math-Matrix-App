e689532965fc2e8bf37369834131191c35824ad0
rwema3 <rwemadominik3@gmail.com>
2022-02-22 10:09:19 +0200

    Update math_util.dart

--- lib/src/math_util.dart
+++ lib/src/math_util.dart
@@ -358,7 +358,7 @@ pression = MathUtil.getMultiplySignExp(1, 10);
           }
         } else {
           switch (sign) {
-            case "+":
+            ":
               expression = MathUtil.getPlusSignExp(min, max);
               break;
             case "-":
