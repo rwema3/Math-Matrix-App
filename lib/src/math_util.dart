95b379c61a209b4a0f6e89781a0953e376c3f5f0
rwema3 <rwemadominik3@gmail.com>
2022-02-22 10:13:33 +0200

    Update math_util.dart

--- lib/src/math_util.dart
+++ lib/src/math_util.dart
@@ -361,7 +361,7 @@ pression = MathUtil.getMultiplySignExp(1, 10);
     return list;
   
     int min = 1 ? 1 : ( level) - 5; //1 5 10 15 20 25
-    int  * level20 
+    int  * leve
   
     while (list.length < count) {
       MathUtil.generateRandomSign1(count - list.length).forEach((String sign) {
