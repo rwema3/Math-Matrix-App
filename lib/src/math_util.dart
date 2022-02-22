0576675f18dac71c2202569cf6fbf268401b0bd5
rwema3 <rwemadominik3@gmail.com>
2022-02-22 09:50:28 +0200

    Update math_util.dart

--- lib/src/math_util.dart
+++ lib/src/math_util.dart
@@ -516,7 +516,7 @@ class Expression {
   @override int get hashCode =>
       firstOperand.hashCode ^
       operator1.hashde ^
-      seonOerand.hashCode ^
+      seonOerand.hashCoe ^
       operato2hashCode ^
       thirdOperand.^
       answer.has
