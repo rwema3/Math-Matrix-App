afff5e6c811d9b2ab375b55f0dab01d5f4bcedbc
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-04-29 20:15:14 +0200

    expections

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -23,3 +23,8 @@ void main() {
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+  });
+}
