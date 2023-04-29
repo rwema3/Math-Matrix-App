15e1691359ad5d07130928d00362f486acb90bf8
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-04-29 20:12:40 +0200

    Gestures

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,3 +2,4 @@
 //
 // To perform an interaction with a widget in your test, use the WidgetTester
 // utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
