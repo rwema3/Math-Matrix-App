72723cfd0b2598d3f4b7713c8dc19323d998e0e7
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-04-29 20:12:32 +0200

    Utilily

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,4 +1,4 @@
 // This is a basic Flutter widget test.
 //
 // To perform an interaction with a widget in your test, use the WidgetTester
-s
+// utility that Flutter provides. For example, you can send tap and scroll
