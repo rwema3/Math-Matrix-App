7b6224dab61eea5773bbe873db1330a538c8a1de
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-04-29 20:14:42 +0200

    expect find.text

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -17,3 +17,6 @@ void main() {
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
