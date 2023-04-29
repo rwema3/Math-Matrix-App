87d0f82d010590f765bffeca224c63c2daa93db4
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-04-29 20:07:49 +0200

    Imports

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mathgame/src/ui/dashboard/dashboard_view_model.dart';
+import 'package:mathgame/src/service/dialog_service.dart';
+import 'package:mathgame/src/service/navigation_service.dart';
+import 'package:mathgame/src/core/router.dart';
+import 'package:mathgame/src/ui/dashboard/dashboard_view.dart';
+import 'package:mathgame/src/ui/common/dialog_manager.dart';
+import 'package:mathgame/src/core/app_constant.dart';
