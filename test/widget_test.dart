55b0e42f12957dc1adc4b45def25befb1289d556
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-04-29 20:12:47 +0200

    Trees

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,3 +3,4 @@
 // To perform an interaction with a widget in your test, use the WidgetTester
 // utility that Flutter provides. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
