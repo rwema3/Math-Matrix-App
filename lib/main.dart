b9f25711a67d9d9f8a1c70ff9bf6767cfd3eaedd
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-06-15 00:18:05 +0200

    setting Preferred Orientations

--- lib/main.dart
+++ lib/main.dart
@@ -26,3 +26,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 //    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
