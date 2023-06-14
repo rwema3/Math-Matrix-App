b8b112355c66ea529d03bbea1ffb5066e0f55ebd
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-06-15 00:15:57 +0200

    Imports- services, get and ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,4 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mathgame/src/ui/dashboard/dashboard_view_model.dart';
