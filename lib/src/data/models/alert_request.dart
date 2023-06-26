64accdf508230abbc702aea15deb2aeaa977876e
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-06-26 16:50:23 +0200

    Updating Mathgame to null safety

--- lib/src/data/models/alert_request.dart
+++ lib/src/data/models/alert_request.dart
@@ -1,12 +1,11 @@
-
 import 'package:mathgame/src/core/app_constant.dart';
 
 class AlertRequest {
   final GameCategoryType gameCategoryType;
-  final String type;
-  final double score;
-  final double coin;
-  final bool isPause;
+  final String? type;
+  final double? score;
+  final double? coin;
+  final bool? isPause;
 
-  AlertRequest({this.type,this.gameCategoryType, this.score, this.coin, this.isPause});
+  AlertRequest({this.type, this.gameCategoryType, this.score, this.coin, this.isPause});
 }
