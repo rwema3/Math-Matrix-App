06d713369ece6f0f5eafa212f850b865ae073dca
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-06-26 17:01:24 +0200

    null safety for alert response

--- lib/src/data/models/alert_response.dart
+++ lib/src/data/models/alert_response.dart
@@ -1,7 +1,7 @@
 class AlertResponse {
-  final bool exit;
-  final bool restart;
-  final bool play;
+  final bool? exit;
+  final bool? restart;
+  final bool? play;
 
   AlertResponse({this.exit, this.restart, this.play});
 }
