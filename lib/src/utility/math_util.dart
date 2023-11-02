97453109863a3c509ebeef12515daa9731a1566e
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-11-02 23:14:50 +0200

    Missing return

--- lib/src/utility/math_util.dart
+++ lib/src/utility/math_util.dart
@@ -1,3 +1,12 @@
 import 'dart:math';
 
 class MathUtil {
+  // ignore: missing_return
+  static int evaluate(int x1, String sign, int x3) {
+    switch (sign) {
+      case "+":
+        return x1 + x3;
+      case "-":
+        return x1 - x3;
+      case "*":
+        return x1 * x3;
