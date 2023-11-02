cce13fec5519bca5cbaa3b70d43d06141f2e6d84
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-11-02 23:19:42 +0200

    while loop

--- lib/src/utility/math_util.dart
+++ lib/src/utility/math_util.dart
@@ -354,3 +354,8 @@ class MathUtil {
     int min = level == 1 ? 1 : (5 * level) - 5; //1 5 10 15 20 25
     int max = level == 1 ? 10 : (10 * level); //10 20 30 40 50 60
     print("$min $max");
+    while (list.length < count) {
+      MathUtil.generateRandomSign1(count - list.length).forEach((String sign) {
+        Expression expression;
+        if (level <= 2) {
+          switch (sign) {
